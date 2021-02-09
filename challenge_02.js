62881560afd471a155ca9f10e632cea86dcf74c5
Igor <igor.lischewski@web.de>
2021-02-10 00:17:22 +0100

    challenge_02_switch_lösung

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,37 @@ Alter 13-17 // Cola
 */
 
 
+let name1, age;
+name1 = prompt("Please enter your name: ");
+age = parseInt(prompt("Please enter your age: "));
+console.log("Name: " + name1 + " // " + "Age: " + age);
+
+let milk, juice, cola, whisky;
+
+milk = (age >= 0, age <= 5);
+juice = (age >= 6, age <= 12);
+cola = (age >= 13, age <= 17);
+whisky = (age >= 18, age <= 117);
+
+
+switch (true) {
+    case (milk):
+        console.log("Hi, at this age milk is your best friend!");
+        break;
+
+    case (juice):
+        console.log("Hi, now you can drink juice besides milk!");
+        break;
+
+    case (cola):
+        console.log("You can drink cola now, but better drink juice or milk!");
+        break;
+
+    case (whisky):
+        console.log("Whisky and Cola Queen of Danc floor!");
+        break;
+
+    default:
+        console.log("World record broken!")
+        break;
+}
